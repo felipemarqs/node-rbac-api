cab04a43b39d6296952dd0e7ed859e22196bbea8
Felipe Marques <felipe.marqs@outlook.com>
2024-01-27 15:40:31 +0000

    feat: :sparkles: SignUp Controller Created

---
+++ api/src/application/controllers/SignUpController.ts
@@ -0,0 +1,29 @@
+import { IController, IRequest, IResponse } from "../interfaces/IController";
+import { ZodError, z } from "zod";
+import { SignUpUseCase } from "../useCases/SignUpUseCase";
+
+const schema = z.object({
+  name: z.string().min(1),
+  email: z.string().email().min(1),
+  password: z.string().min(6),
+});
+
+export class SignUpController implements IController {
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { name, email, password } = schema.parse(body);
+
+      const signUpUseCase = new SignUpUseCase();
+
+      signUpUseCase.execute({ name, email, password });
+
+      return { statusCode: 204, body: null };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return { statusCode: 400, body: error.issues };
+      }
+
+      throw error;
+    }
+  }
+}
